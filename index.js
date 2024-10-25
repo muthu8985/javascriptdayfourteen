e1f8dd0e62218974ecd5c5768841609ecdf79c3d
Muthu Kumar <muthukumarv8985@gmail.com>
2024-10-25 12:14:05 +0530

    second commit

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 // task of set
 //  new set
+console.log("---------------task-------------");
 let groceries = new Set(["salt", "sugar", "paste", "pasta", "soap"]);
 // size of set
 console.log(groceries.size);
-// add set 
-addgro = groceries.add("oil", "shampoo", "tomato");
+// add set
+//  let addgro = groceries.add("oil", "shampoo", "tomato");
+
+let addgro = groceries.add("oil").add("shampoo").add("tomato");
 console.log(addgro);
 // del set
-delgro = groceries.delete("tomato");
+let delgro = groceries.delete("tomato");
+
 console.log(delgro);
+console.log(groceries);
 //  list  all set
-ligro="";
-for (const x of groceries){
-    ligro += x+"<br>"
+ligro = "";
+for (const x of groceries) {
+  ligro += x + "<br>";
 }
-console.log(ligro)
-// has set 
-hasgro= groceries.has("tomato");
+console.log(ligro);
+// has set
+let hasgro = groceries.has("tomato");
+console.log(hasgro);
 // entries
-entgro=groceries.entries()
- let txt="";
- for (const g of entgro){
-    txt += g +"<br>"
- }
- console.log(txt)
+let entgro = groceries.entries();
+let txt = "";
+for (const g of entgro) {
+  txt += g + "<br>";
+}
+console.log(txt);
 //   for each
-  let eachtxt ="";
-   groceries.forEach(function (value) {
-    
-    eachtxt += value+"<br>"
-   });
-   console.log(eachtxt)
+let eachtxt = "";
+groceries.forEach(function (value) {
+  eachtxt += value + "<br>";
+});
+console.log(eachtxt);
